463e0fe91c18b9dcc9ec4b1a3cfea0ba56a0d194
AeronDigitalSolutions <aerondigitaldevelopment@gmail.com>
2025-12-17 13:18:34 +0530

    overflow fix

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 // import "";
 import type { AppProps } from "next/app";
+import "@/style/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
